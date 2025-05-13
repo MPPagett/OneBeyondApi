e17e786959d0de23efd89d98540210056f1641f4
MPPagett <mpagett@hotmail.co.uk>
2025-05-13 09:03:08 +0100

    Add Fine and Reservation models and update gitignore.

---
+++ library/Model/Fine.cs
@@ -0,0 +1,13 @@
+﻿namespace OneBeyondApi.Model
+{
+    public class Fine
+    {
+        public int Id { get; set; }
+        public int BorrowerId { get; set; }
+        public decimal Amount { get; set; }
+        public string Reason { get; set; }
+        public DateTime IssuedDate { get; set; }
+
+        public Borrower Borrower { get; set; } = default!;
+    }
+}
---
+++ library/Model/Reservationcs.cs
@@ -0,0 +1,13 @@
+﻿namespace OneBeyondApi.Model
+{
+    public class Reservationcs
+    {
+        public int Id { get; set; }
+        public int BorrowerId { get; set; }
+        public int BookId { get; set; }
+        public DateTime ReservedDate { get; set; }
+
+        public Borrower Borrower { get; set; } = default!;
+        public Book Book { get; set; } = default!;
+    }
+}
